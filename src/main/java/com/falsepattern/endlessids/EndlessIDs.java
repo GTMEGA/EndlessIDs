d1666b582f48a9c89f68439ac5255580f54b9040
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-07-01 15:02:43 +0200

    Anti ID Conflict mod compatibility patch

--- src/main/java/com/falsepattern/endlessids/EndlessIDs.java
+++ src/main/java/com/falsepattern/endlessids/EndlessIDs.java
@@ -1,6 +1,10 @@
 package com.falsepattern.endlessids;
 
+import code.elix_x.coremods.antiidconflict.managers.BiomesManager;
+import com.falsepattern.endlessids.constants.ExtendedConstants;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,4 +15,15 @@ import org.apache.logging.log4j.Logger;
      dependencies = "required-after:falsepatternlib@[0.8.2,);")
 public class EndlessIDs {
     public static final Logger LOG = LogManager.getLogger(Tags.MODNAME);
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent e) {
+        if (Loader.isModLoaded("antiidconflict")) {
+            antiIdConflictLatePatch();
+        }
+    }
+
+    private static void antiIdConflictLatePatch() {
+        BiomesManager.conflicts = new BiomesManager.ConflictingBiomes[ExtendedConstants.biomeIDCount];
+    }
 }
