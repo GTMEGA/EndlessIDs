e44229300aaa0ae238778a68d827e8450f6079dd
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-07-18 21:46:30 +0200

    no more atum mixins; remove it from the mod targeting system

--- src/main/java/com/falsepattern/endlessids/mixin/plugin/TargetedMod.java
+++ src/main/java/com/falsepattern/endlessids/mixin/plugin/TargetedMod.java
@@ -15,7 +15,6 @@ public enum TargetedMod implements ITargetedMod {
     ATG("Alternate Terrain Generation", true, startsWith("atg")),
     ANTIQUEATLAS("Antique Atlas", true, startsWith("antiqueatlas")),
     ANTIIDCONFLICT("Anti ID Conflict", true, startsWith("antiidconflict")),
-    ATUM("Atum", true, startsWith("atum-")),
     BOP("Biomes O' Plenty", true, startsWith("biomesoplenty-")),
     BIOMETWEAKER("Biome Tweaker", true, startsWith("biometweaker-")),
     BIOMEWAND("Biome Wand", true, startsWith("1.7.10-biome-wand-").or(startsWith("biome-wand"))),
