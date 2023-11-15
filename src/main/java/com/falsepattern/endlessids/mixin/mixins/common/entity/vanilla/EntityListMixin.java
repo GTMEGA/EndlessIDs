c052edf9120ce41db36d955c99bd0aa81cd4ddba
FalsePattern <me@falsepattern.com>
2023-11-15 19:52:26 +0100

    remove entity ID experiments

--- src/main/java/com/falsepattern/endlessids/mixin/mixins/common/entity/vanilla/EntityListMixin.java
+++ src/main/java/com/falsepattern/endlessids/mixin/mixins/common/entity/vanilla/EntityListMixin.java
@@ -14,36 +14,11 @@ import net.minecraft.entity.EntityList;
 
 @Mixin(EntityList.class)
 public abstract class EntityListMixin {
-    @Shadow
-    public static void addMapping(Class p_75618_0_, String p_75618_1_, int p_75618_2_) {
-    }
-
-    @Shadow
-    public static void addMapping(Class p_75614_0_, String p_75614_1_, int p_75614_2_, int p_75614_3_, int p_75614_4_) {
-
-    }
 
     @ModifyConstant(method = "addMapping(Ljava/lang/Class;Ljava/lang/String;I)V",
                     constant = @Constant(intValue = VanillaConstants.maxEntityID),
                     require = 1)
     private static int extendRange(int constant) {
         return ExtendedConstants.maxEntityID;
-    }
-
-    @Redirect(method = "<clinit>",
-              at = @At(value = "INVOKE",
-                       target = "Lnet/minecraft/entity/EntityList;addMapping(Ljava/lang/Class;Ljava/lang/String;I)V"),
-              require = 1)
-    private static void messUpMappings1(Class p_75618_0_, String p_75618_1_, int p_75618_2_) {
-        addMapping(p_75618_0_, p_75618_1_, p_75618_2_ * 100);
-    }
-
-
-    @Redirect(method = "<clinit>",
-              at = @At(value = "INVOKE",
-                       target = "Lnet/minecraft/entity/EntityList;addMapping(Ljava/lang/Class;Ljava/lang/String;III)V"),
-              require = 1)
-    private static void messUpMappings2(Class p_75614_0_, String p_75614_1_, int p_75614_2_, int p_75614_3_, int p_75614_4_) {
-        addMapping(p_75614_0_, p_75614_1_, p_75614_2_ * 100, p_75614_3_, p_75614_4_);
     }
 }
