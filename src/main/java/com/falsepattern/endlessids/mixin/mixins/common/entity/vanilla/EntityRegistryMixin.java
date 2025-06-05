b94ae7f3e3d4adaf980a020a773e9bbd97774e27
FalsePattern <me@falsepattern.com>
2025-06-05 11:35:10 +0200

    update license date

--- src/main/java/com/falsepattern/endlessids/mixin/mixins/common/entity/vanilla/EntityRegistryMixin.java
+++ src/main/java/com/falsepattern/endlessids/mixin/mixins/common/entity/vanilla/EntityRegistryMixin.java
@@ -1,7 +1,7 @@
 /*
  * EndlessIDs
  *
- * Copyright (C) 2022-2024 FalsePattern
+ * Copyright (C) 2022-2025 FalsePattern, The MEGA Team
  * All Rights Reserved
  *
  * The above copyright notice, this permission notice and the words "MEGA"
@@ -9,13 +9,12 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, only version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
